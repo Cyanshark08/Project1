a2d484201ccdfbfcdde9bfc28ec9b2cc0eb7d4e9
Landon Mendoza <landonmendoza@outlook.com>
2025-08-18 23:02:08 -0700

    Calculation Manager
    - Created static Method declarations for Calculation Manager

--- Project1/src/CalculationManager.h
+++ Project1/src/CalculationManager.h
@@ -17,7 +17,30 @@ enum class EQuartile
 class CalculationManager
 {
 public:
-	
+	static float FindMin(const DynamicDataSet& p_DataSet);
+	static float FindMax(const DynamicDataSet& p_DataSet);
+	static float FindRange(const DynamicDataSet& p_DataSet);
+	static float FindSize(const DynamicDataSet& p_DataSet);
+	static float FindSum(const DynamicDataSet& p_DataSet);
+	static float FindMean(const DynamicDataSet& p_DataSet);
+	static float FindMedian(const DynamicDataSet& p_DataSet);
+	static DynamicDataSet FindMode(const DynamicDataSet& p_DataSet);
+	static float FindStandardDeviation(const DynamicDataSet& p_DataSet);
+	static float FindVariance(const DynamicDataSet& p_DataSet);
+	static float FindMidRange(const DynamicDataSet& p_DataSet);
+	static std::tuple<float, float, float> FindQuartiles(const DynamicDataSet& p_DataSet);
+	static float FindInterquartileRange(const DynamicDataSet& p_DataSet);
+	static DynamicDataSet FindOutliers(const DynamicDataSet& p_DataSet);
+	static float FindSumOfSquares(const DynamicDataSet& p_DataSet);
+	static float FindMeanAbsoluteDeviation(const DynamicDataSet& p_DataSet);
+	static float FindRootMeanSquare(const DynamicDataSet& p_DataSet);
+	static float FindStandardErrorOfMean(const DynamicDataSet& p_DataSet);
+	static float FindSkewness(const DynamicDataSet& p_DataSet);
+	static float FindKurtosis(const DynamicDataSet& p_DataSet);
+	static float FindKurtosisExcess(const DynamicDataSet& p_DataSet);
+	static float FindCoefficientOfVariation(const DynamicDataSet& p_DataSet);
+	static float FindRelativeStandardDeviation(const DynamicDataSet& p_DataSet);
+	static std::string GetFrequencyTable(const DynamicDataSet& p_DataSet);
 
 private:
 	static ECaluclatorState s_State;
